4a87fcce7e7f916080e4784c49bb13cf14f34c37
diggzhang <diggzhang@gmail.com>
2018-10-17 19:14:58 +0800

    collections中namedtuple可以做到命名元组
    为什么要命名元组？使用下表读结构，容易混淆。

--- read_csv.py
+++ read_csv.py
@@ -1,7 +1,10 @@
+from collections import namedtuple
 import csv 
 
 with open('stocks.csv') as f:
     f_csv = csv.reader(f)
     headers = next(f_csv)
-    for row in f_csv:
+    Row = namedtuple('Row', headers)
+    for r in f_csv:
+        row = Row(*r)
         print(row)
